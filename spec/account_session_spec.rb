6f0c0724e0513abbe0ee05a50f8846e3bbe7f5e2
Lars <larsfinlay@googlemail.com>
2018-02-28 13:26:08 +0000

    Account session has a details attribute that is a hash

--- spec/account_session_spec.rb
+++ spec/account_session_spec.rb
@@ -2,8 +2,18 @@ require_relative '../lib/account_session.rb'
 
 describe AccountSession do
 
-  it 'pretest' do
-    expect(1).to eq(1)
+  subject { described_class.new({
+    "id"=>"testid",
+    "firstname" => "testname",
+    "lastname" => "testlastname",
+    "email" => "testemail",
+    "telephone" => "7357"
+    })}
+
+  describe '#instantiation' do
+    it 'Should be instantiated with details that are set to an attribute "details"' do
+      expect(subject.details).to be_a Hash
+    end
   end
 
 end
