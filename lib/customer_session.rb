f70a8671328e8e296930df8d4bfdc2392b283936
Lars <larsfinlay@googlemail.com>
2018-02-28 14:17:50 +0000

    Customer session is created with an account attribute that can be set :: green

--- lib/customer_session.rb
+++ lib/customer_session.rb
@@ -1,3 +1,8 @@
 # Created for customers who wish to search the base to seek debted accounts
 class CustomerSession
+  attr_reader :accounts
+
+  def initialize(accounts)
+    @accounts = accounts
+  end
 end
