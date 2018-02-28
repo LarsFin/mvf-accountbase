ba032abe24c20b59135fdd385b80438acd71ef31
Lars <larsfinlay@googlemail.com>
2018-02-28 16:51:10 +0000

    create_customer_session method should create instance of customer_session_class attr :: red

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MVF API
 An application to implement the following user stories;
 
-priority -> **4** √
+priority -> **4** √ 
 ```
 As an account holder,
 I want to check my balance,
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/app.rb'
 describe App do
 
   let(:account_session_class) { double('account_session_class', new: 'new called') }
-  let(:customer_session_class) { double('customer_session_class') }
+  let(:customer_session_class) { double('customer_session_class', new: 'new called') }
   subject { described_class.new(account_session_class, customer_session_class) }
 
   describe 'App instantiation' do
@@ -42,6 +42,13 @@ describe App do
     it "Should call instantiation with the details of the user matching the id" do
       expect(account_session_class).to receive(:new).with({"id" => "861fc585-3313-4928-891d-c8711dfe3f8a","firstname" => "Izayah","lastname" => "Hayden","email" => "Izaya.HAY6958@yopmail.com","telephone" => "01035 837401","balance" => "8,759.84"})
       subject.create_account_session('861fc585-3313-4928-891d-c8711dfe3f8a')
+    end
+  end
+
+  describe '#create_customer_session' do
+    it 'Should call the instantiation of a new instance of "customer_session_class"' do
+      expect(customer_session_class).to receive(:new)
+      subject.create_customer_session
     end
   end
 
