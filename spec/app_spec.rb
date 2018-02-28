4e86136fdcd4955df85f5c4cbb56b4b1bea2a146
Lars <larsfinlay@googlemail.com>
2018-02-28 10:38:50 +0000

    Can read real data from accounts :: red

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,8 +4,14 @@ describe App do
 
   subject { described_class.new }
 
-  it 'App should be initialised with an array set to an attirbute "accounts"' do
-    expect(subject.accounts).to be_a Array
+  describe '#App_accounts' do
+    it 'App should be initialised with an array set to an attirbute "accounts"' do
+      expect(subject.accounts).to be_a Array
+    end
+
+    it 'Should be able to read real data from an account in accounts array' do
+      expect(subject.accounts[0][:firstname]).to equal("Izayah")
+    end
   end
 
 end
