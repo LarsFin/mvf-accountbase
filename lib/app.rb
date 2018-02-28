fbde8b82a1c87c7d2ce6bc0f3cfbbacf777b416a
Lars <larsfinlay@googlemail.com>
2018-02-28 11:12:53 +0000

    Verify id test :: green

--- lib/app.rb
+++ lib/app.rb
@@ -7,4 +7,8 @@ class App
     file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
     @accounts = JSON.parse(file)['accounts']
   end
+
+  def verify_id(id)
+    accounts.any? { |k, v| k == 'id' && v == id }
+  end
 end
