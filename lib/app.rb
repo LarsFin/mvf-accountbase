0476e23649013c6a23757093d9c0a8ed444ea5a0
Lars <larsfinlay@googlemail.com>
2018-02-28 10:47:22 +0000

    Can read data from accounts :: refactored

--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,7 @@ class App
   attr_reader :accounts
 
   def initialize
-    @accounts = JSON.parse(File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json'))["accounts"]
+    file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
+    @accounts = JSON.parse(file)['accounts']
   end
 end
