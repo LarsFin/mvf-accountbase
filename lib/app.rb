78dea1cf48ce38c7e89022f508172dddb70efdec
Lars <larsfinlay@googlemail.com>
2018-02-28 11:38:33 +0000

    App has account_session_class attr that can be set :: green

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,12 @@
 require 'json'
 # App to be used as the controller by the user
 class App
-  attr_reader :accounts
+  attr_reader :accounts, :account_session_class
 
-  def initialize()
+  def initialize(account_session_class)
     file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
     @accounts = JSON.parse(file)['accounts']
+    @account_session_class = account_session_class
   end
 
   def verify_id(id)
