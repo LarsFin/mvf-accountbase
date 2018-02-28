9c361b9757a3e7f332a5a2753563d1373efe2152
Lars <larsfinlay@googlemail.com>
2018-02-28 20:27:53 +0000

    app_class has new called with args :: red

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,6 +1,9 @@
 # For the user to interact with the app
 class Controller
-  def initialize(app_class)
-    app = app_class.new
+  def initialize(app_class,
+                 account_session_class,
+                 customer_session_class,
+                 current_company_guid)
+    @app = app_class.new
   end
 end
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -3,12 +3,12 @@ require_relative '../lib/controller'
 describe Controller do
 
   let(:app_class) { double('app_class', new: 'new called') }
-  subject { described_class.new(app_class) }
+  subject { described_class.new(app_class, '1', '2', '3') }
 
   describe '#instantiation' do
     it 'should be initialised with an app_class which it creates a new instance of' do
-      expect(app_class).to receive(:new)
-      Controller.new(app_class)
+      expect(app_class).to receive(:new).with('1', '2', '3')
+      Controller.new(app_class, '1', '2', '3')
     end
   end
 
