6f40046e81e2af0f378fa0955c8ba06437760ab7
Lars <larsfinlay@googlemail.com>
2018-02-28 13:54:30 +0000

    Information method retrieves the contact information of the account holder :: green

--- lib/account_session.rb
+++ lib/account_session.rb
@@ -10,6 +10,13 @@ class AccountSession
     get_detail('balance')
   end
 
+  def information
+    [get_detail("firstname"),
+     get_detail("lastname"),
+     get_detail("email"),
+     get_detail("telephone")]
+  end
+
   private
 
   def get_detail(param)
