012eb186b0a55d56fc148c0ebae550f7ab52a44b
Lars <larsfinlay@googlemail.com>
2018-02-28 17:26:52 +0000

    Request body for accounts :: red

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,12 @@ SimpleCov.start
 
 RSpec.configure do |config|
 
+  config.before(:each) do
+    stub_request(:get, /mvf-devtest-s3api.s3-eu-west-1.amazonaws.com/).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: '{"accounts":[{"id":"861fc585-3313-4928-891d-c8711dfe3f8a","firstname":"Izayah","lastname":"Hayden","email":"Izaya.HAY6958@yopmail.com","telephone":"01035 837401","balance":"8,759.84"}}', headers: {})
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
