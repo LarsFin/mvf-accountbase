687253e6985cc5d097640ac0dbc54ce6f0a5948b
Lars <larsfinlay@googlemail.com>
2018-02-28 16:36:11 +0000

    App has customer_session_class attribute :: green

--- lib/app.rb
+++ lib/app.rb
@@ -3,12 +3,13 @@ require_relative 'customer_session'
 require 'json'
 # App to be used as the controller by the user
 class App
-  attr_reader :accounts, :account_session_class
+  attr_reader :accounts, :account_session_class, :customer_session_class
 
-  def initialize(account_session_class)
+  def initialize(account_session_class, customer_session_class)
     file = File.read('a4a06bb0-3fbe-40bd-9db2-f68354ba742f.json')
     @accounts = JSON.parse(file)['accounts']
     @account_session_class = account_session_class
+    @customer_session_class = customer_session_class
   end
 
   def create_account_session(id)
